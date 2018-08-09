e8dd9fc2d7e2edb11f3d2244531740f523a59fe6
slava-n <novozhilovu@gmail.com>
2018-08-09 21:37:12 +0300

    homework_paths_folders_done

--- VK_unique_groups/get_token.py
+++ VK_unique_groups/get_token.py
@@ -1,14 +1,14 @@
 from urllib.parse import urlencode, urlparse
 
 
-app_id = 6652821
+app_id = 6655967
 auth_url = 'https://oauth.vk.com/authorize'
 api_ver = '5.80'
 
 auth_data = dict(client_id=app_id,
                  redirect_uri='https://oauth.vk.com/blank.html',
                  display='page',
-                 scope='friends, status',
+                 scope='friends, status, groups',
                  response_type='token',
                  v=api_ver)
 
